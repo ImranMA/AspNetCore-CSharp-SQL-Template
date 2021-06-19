2d0f8d2c20da8b4ac8438c3f883ea90f09bb3bff
Imran Arshad <imran30758@hotmail.com>
2021-06-19 17:19:37 +1000

    Add project files.

---
+++ Template/DB/StudentDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Template.Models;
+
+namespace Template.DB
+{
+    public class StudentDBContext : DbContext
+    {
+        public StudentDBContext(DbContextOptions<StudentDBContext> options)
+          : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Template/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Template.Models
+{
+    public class Student
+    {
+        [Key]
+        public string StudentId { get; set; }
+
+        public string Name { get; set; }        
+    }
+}
